e2e50d31ffca9e827c8d9213ff65099551caa96f
Leo <goldcarrotminer@gmail.com>
2023-12-26 11:43:51 +0800

    feat: dynamically generate page title based on pathname

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
-import { useState, lazy, Suspense } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { useState, lazy, Suspense, useEffect } from "react";
+import { HashRouter as Router, Route, Switch, useLocation } from "react-router-dom";
 import Nav from "./navbar";
 import LinearProgress from "@mui/material/LinearProgress";
 
@@ -13,6 +13,16 @@ const ImageUrl = lazy(() => import("./pages/imageUrl"));
 const WhatsApp = lazy(() => import("./pages/whatsapp"));
 const GroceryList = lazy(() => import("./pages/groceryList"));
 
+const RouterContainer = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.title = pathname === "/" ? "React App" : pathname.split('/')[1].replace(/([A-Z])/g, " $1")
+  }, [pathname])
+  return <>
+    {children}
+  </>
+}
 export default function Routes() {
   const [homePg, setHomePg] = useState(false);
 
@@ -20,19 +30,21 @@ export default function Routes() {
     <Router>
       <Nav homePg={homePg} setHomePg={setHomePg} />
       <Suspense fallback={<LinearProgress />}>
-        <Switch>
-          <Route path="/" exact>
-            <Home setHomePg={setHomePg} />
-          </Route>
-          <Route path="/TodoApp" component={TodoApp} />
-          <Route path="/ColorApp" component={ColorApp} />
-          <Route path="/Covid" component={Covid} />
-          <Route path="/Curl" component={Curl} />
-          <Route path="/WordHack" component={WordHack} />
-          <Route path="/ImageUrl" component={ImageUrl} />
-          <Route path="/Whatsapp" component={WhatsApp} />
-          <Route path="/GroceryList" component={GroceryList} />
-        </Switch>
+
+        <RouterContainer>
+          <Switch>
+            <Route path="/" exact>
+              <Home setHomePg={setHomePg} />
+            </Route>
+            <Route path="/TodoApp" component={TodoApp} />
+            <Route path="/ColorApp" component={ColorApp} />
+            <Route path="/Covid" component={Covid} />
+            <Route path="/Curl" component={Curl} />
+            <Route path="/WordHack" component={WordHack} />
+            <Route path="/ImageUrl" component={ImageUrl} />
+            <Route path="/Whatsapp" component={WhatsApp} />
+            <Route path="/GroceryList" component={GroceryList} />
+          </Switch></RouterContainer>
       </Suspense>
     </Router>
   );
