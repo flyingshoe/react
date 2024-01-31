a1d0df7f34f0e346221ab81aadf94d4038dcd702
Leo <goldcarrotminer@gmail.com>
2024-01-31 17:43:49 +0800

    fix: replace CRA service worker with vite service worker plugin

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       src: "/src",
     },
   },
-  server: {
-    host: 'localhost',
-    port: 3000,
-  }
+  // server: {
+  //   host: 'localhost',
+  //   port: 3000,
+  // }
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
