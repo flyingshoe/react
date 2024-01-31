7ae377d584ea8fa603c5bd9308563e6ed20a8825
Leo <goldcarrotminer@gmail.com>
2024-02-01 00:00:59 +0800

    feat: add config for module federation to function as a host app

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +21,13 @@ export default defineConfig({
       // devOptions: {
       //   enabled: true,
       // },
+    }),
+    federation({
+      name: "reactHost",
+      filename: "remoteEntry.js",
+      remotes: {
+        stockApp: "http://localhost:4173/assets/remoteEntry.js",
+      },
     }),
   ],
 });
