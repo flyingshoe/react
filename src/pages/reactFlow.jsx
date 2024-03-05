39a7999f5178fe3155807bb239dd82bc186b8654
Leo <goldcarrotminer@gmail.com>
2024-03-05 13:23:13 +0800

    feat: drag points to add new nodes

--- src/pages/reactFlow.jsx
+++ src/pages/reactFlow.jsx
@@ -1,17 +1,90 @@
-import React from "react";
-import ReactFlow from "reactflow";
+import React, { useCallback, useRef } from "react";
+import ReactFlow, {
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  useReactFlow,
+  ReactFlowProvider,
+} from "reactflow";
 import "reactflow/dist/style.css";
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "React" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "Svelte" } },
+  { id: "1", position: { x: 50, y: 50 }, data: { label: "React" } },
+  { id: "2", position: { x: 150, y: 150 }, data: { label: "Svelte" } },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2", type: "step" }];
 
-export default function Diagram() {
+function Diagram() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const reactFlowWrapper = useRef(null);
+  const connectingNodeId = useRef(null);
+
+  const { screenToFlowPosition } = useReactFlow();
+  const onConnect = useCallback((params) => {
+    // reset the start node on connections
+    connectingNodeId.current = null;
+    setEdges((eds) => addEdge(params, eds));
+  }, []);
+
+  const onConnectStart = useCallback((_, { nodeId }) => {
+    connectingNodeId.current = nodeId;
+  }, []);
+
+  const onConnectEnd = useCallback(
+    (event) => {
+      if (!connectingNodeId.current) return;
+
+      const targetIsPane = event.target.classList.contains("react-flow__pane");
+
+      if (targetIsPane) {
+        // we need to remove the wrapper bounds, in order to get the correct position
+        const id = Date.now().toString();
+        const newNode = {
+          id,
+          position: screenToFlowPosition({
+            x: event.clientX,
+            y: event.clientY,
+          }),
+          data: { label: `Node ${id}` },
+          origin: [0.5, 0.0],
+        };
+
+        setNodes((nds) => nds.concat(newNode));
+        setEdges((eds) =>
+          eds.concat({ id, source: connectingNodeId.current, target: id })
+        );
+      }
+    },
+    [screenToFlowPosition]
+  );
+
   return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    <div className="w-full h-full bg-blue-50" ref={reactFlowWrapper}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onConnectStart={onConnectStart}
+        onConnectEnd={onConnectEnd}
+        fitView
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
     </div>
+  );
+}
+
+export default function DiagramProvider() {
+  return (
+    <ReactFlowProvider>
+      <Diagram />
+    </ReactFlowProvider>
   );
 }
